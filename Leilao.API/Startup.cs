62ba0bd2dc8197fb00831bc462ef67cbc1a2ab7e
Lucas de Paula <lucasdepaula@bne.com.br>
2020-09-11 03:36:41 -0300

    cors

--- Leilao.API/Startup.cs
+++ Leilao.API/Startup.cs
@@ -26,6 +26,13 @@ namespace Leilao.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    b => b.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +48,8 @@ namespace Leilao.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
